48f6cf94c886dbf484852e2160a6bfbf3400f1f6
Bahadır Büber <54572106+bahadirbuber@users.noreply.github.com>
2021-06-22 22:18:49 +0300

    Initial Commit

---
+++ MavsdkSwiftSwiftui/MavsdkSwiftSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MavsdkSwiftSwiftuiApp.swift
+//  MavsdkSwiftSwiftui
+//
+//  Created by Bahadır Büber on 22.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct MavsdkSwiftSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

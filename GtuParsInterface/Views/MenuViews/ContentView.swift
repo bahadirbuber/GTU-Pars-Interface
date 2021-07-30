34126fbc50a6544b22418cec4ad5357ea7a5bb2b
Bahadır Büber <54572106+bahadirbuber@users.noreply.github.com>
2021-07-30 14:16:40 +0300

    View değişimi

---
+++ GtuParsInterface/Views/MenuViews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GtuParsInterface
+//
+//  Created by Bahadır Büber on 22.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var telemetry = TelemetryViewModel()
+    var body: some View {
+        GeometryReader{ geometry in
+            MainView(geoWidth: 3.8*(geometry.size.width/16), geoHeight: 3.8*(geometry.size.height/9))
+        }.frame(minWidth: 1280, idealWidth: 1600, maxWidth: .infinity, minHeight: 720, idealHeight: 900, maxHeight: .infinity, alignment: .center)
+        .background(Image("background2"))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

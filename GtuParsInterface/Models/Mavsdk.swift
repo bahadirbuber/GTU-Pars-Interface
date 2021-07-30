e4892cb504901713364a4aa24ec0f963b353e6cd
Bahadır Büber <54572106+bahadirbuber@users.noreply.github.com>
2021-07-30 14:35:20 +0300

    Yorumlar detaylandırıldı.

--- GtuParsInterface/Models/Mavsdk.swift
+++ GtuParsInterface/Models/Mavsdk.swift
@@ -17,6 +17,17 @@ class Mavsdk: ObservableObject {
     var drone: Drone
     private init() {
         drone = Drone(address: "0.0.0.0",port: 50051)
+        /*Programı çalıştırmadan önce MAVProxy ile telemetri bilgisayara bağlıyken USB portunu
+         birden fazla çıkışa vermemiz gerekiyor.
+         */
+        /*
+         sudo mavproxy.py --master=/dev/tty.usbmodem141201 --out=udp:127.0.0.1:14540
+         Yukarıdaki kod USBden alınan girişi localhostta 14540 portlu yeni UDP bağlantısı oluşturur.
+         */
+        /*Daha sonra yeni çıkıştan Mavsdk Server başlatıyoruz.
+         Terminale mavsdk_server yazdığımızda 14540 portundan aldığı bağlantıyla 50051 portlu yeni
+         Mavsdk Server başlatır.
+         */
         //Programı başlatırken Model ile Mavsdk Server'a bağlanıyoruz.
     }
 }
